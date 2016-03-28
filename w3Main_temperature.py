def3572129be33f1eb935641ab8c875f2a2c029d
OhDakyeong <yds03167@naver.com>
2016-03-28 22:19:36 +0900

    w3Main
    temperature

---
+++ w3Main_temperature.py
@@ -0,0 +1,13 @@
+﻿temp=raw_input("user input temperature: ")
+
+sel=raw_input("F or C: ")
+
+temp=int(temp)
+
+    print ((temp-32)/1.8),"C"
+
+elif(sel=="C"):
+    print ((temp*1.8)+32),"F"
+
+else:
+    print "Input Error"
