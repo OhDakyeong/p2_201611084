c236393e9ca49cba14bca7ae8b939466e54702aa
Oh Dakyeong <yds03167@naver.com>
2016-04-03 16:34:08 +0900

    w5Main_3

---
+++ w5Main_3.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[5]:
+
+userA=raw_input("get userA choice: ")
+userB=raw_input("get userB choice: ")
+print userA, userB
+def GameRSP(userA,userB):
+    if userA==userB:
+        print ("draw")
+    elif userA=="r" and userB=="s" or userA=="s" and userB=="p" or userA=="p" and userB=="r":
+        print("userA wins")
+    elif userA=="s" and userB=="r" or userA=="p" and userB=="s" or userA=="r" and userB=="p":
+        print("userB wins")
+    else:
+        print ("Please check your choice.")
+GameRSP(userA,userB)
+
