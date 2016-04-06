946f069a7ca64d909d9b5e99f2bbf64b59c4d591
Oh Dakyeong <yds03167@naver.com>
2016-04-06 13:41:13 +0900

    w6Main_2
    LeapYear

---
+++ w6Main_2.py
@@ -0,0 +1,9 @@
+﻿year=raw_input("enter year: ")
+year=int(year)
+def LeapYear(year):
+    if (year%4==0) and (year%100!=0 or year%400==0):
+        res="Leap Year"
+    else:
+        res="Not Leap Year"
+    return res
+LeapYear(year)
