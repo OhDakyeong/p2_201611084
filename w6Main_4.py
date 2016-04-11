25db332681e545a860c454f4e63fa398949b78ce
Oh Dakyeong <yds03167@naver.com>
2016-04-11 21:45:25 +0900

    List (4로 나뉘고 5로 나뉘지 않는 1000이하의 수들의 합)
    List (4로 나뉘고 5로 나뉘지 않는 1000이하의 수들의 합)

---
+++ w6Main_4.py
@@ -0,0 +1,26 @@
+﻿"""
+@author odk
+@since 160411
+"""
+
+def sumList(aList):
+    x=list()
+    for i in range(0,aList):
+        if i%4==0 and i%5!=0:
+            x.append(i)
+    mysum=0
+    for l in range(0,len(x)):
+        mysum=mysum+x[l]
+    return mysum
+
+def lab6():
+    """programming is really ha ha ha fun"""
+    aList=1000
+    labsum=sumList(aList)
+    print labsum
+
+def main():
+    lab6()
+
+if __name__=="__main__":
+    main()
