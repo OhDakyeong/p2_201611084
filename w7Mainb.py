5c3a983dc79e64c698ad028c8b5eb5b7f59ed5ea
Oh Dakyeong <yds03167@naver.com>
2016-04-20 13:46:51 +0900

    w7Main_b

---
+++ w7Mainb.py
@@ -0,0 +1,45 @@
+def saveTracks():
+    import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    mytracks=list()
+    t1.speed(1)
+    t1.penup()
+    t1.goto(-400,300)
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(250)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(150)
+    mytracks.append(t1.pos())
+    t1.right(90)
+    t1.fd(300)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(400)
+    mytracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(200)
+    mytracks.append(t1.pos())
+    wn.exitonclick()
+    return mytracks
+
+def replayTracks(mytracks):
+    for t in mytracks:
+        print t
+    
+def lab7_b():
+    mytracks=saveTracks()
+    replayTracks(mytracks)
+    
+def main():
+    lab7_b()
+    
+main()
