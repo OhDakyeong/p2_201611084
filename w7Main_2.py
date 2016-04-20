cbf94bd0cf68d395f5412f4c79c74b57101f0c0b
Oh Dakyeong <yds03167@naver.com>
2016-04-20 23:35:00 +0900

    w7Main_2.py

--- w7Main_2.py
+++ w7Main_2.py
@@ -6,6 +6,7 @@ def saveTracks():
     t1.speed(1)
     t1.penup()
     t1.goto(-400,300)
+    mytracks.append(t1.pos())
     t1.pendown()
     t1.pencolor("Red")
     t1.right(90)
@@ -35,11 +36,12 @@ def replayTracks(mytracks):
     for t in mytracks:
         print t
     
-def lab7_b():
+def lab7_2():
     mytracks=saveTracks()
     replayTracks(mytracks)
     
 def main():
-    lab7_b()
-    
-main()
+    lab7_2()
+ 
+if __name__=="__main__": 
+    main() 
