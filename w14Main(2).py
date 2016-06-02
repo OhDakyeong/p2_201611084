cb3fec3dd89d104647ff311c6d5edfa60e785374
Oh Dakyeong <yds03167@naver.com>
2016-06-02 22:36:29 +0900

    w14Main(2)

---
+++ w14Main(2).py
@@ -0,0 +1,32 @@
+﻿def bow():
+    class Dog(object):
+        def __init__(self,name):
+            self.name=name
+        def getName(self):
+            print("my dog name is ",self.name)
+        def talk(self):
+            print("mung mung")
+        
+    class ShihTzu(object):
+        def talk(self):
+            print("si si")
+        
+    class Maltese(object):
+        def talk(self):
+            print("mal mal")
+
+    mydog=Dog('poppy')
+    mydog.talk()
+    shihtzu=ShihTzu()
+    shihtzu.talk()
+    maltese=Maltese()
+    maltese.talk()
+
+def lab14_2():
+    bow()
+
+def main():
+    lab14_2()
+
+if __name__="__main__":
+    main()
